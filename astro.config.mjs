c6153f774d1a0cbeee1b176109c6886621d1509c
William Bierbower <williambierbower@Mac-3877.lan>
2026-01-15 00:21:55 -0500

    Remove sitemap plugin to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://williambierbower.github.io',
   base: '/waypoint-talks',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx()],
   output: 'static',
   image: {
     service: {
